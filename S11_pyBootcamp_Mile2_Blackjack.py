7a4f700774dac03bac44c53ebf561a1fe2d945ee
rocket7 <mugwump_13@yahoo.com>
2019-01-11 21:01:39 -0500

    first commit

--- S11_pyBootcamp_Mile2_Blackjack.py
+++ S11_pyBootcamp_Mile2_Blackjack.py
@@ -4,6 +4,7 @@
 # https://github.com/Pierian-Data/Complete-Python-3-Bootcamp/blob/master/08-Milestone%20Project%20-%202/01-Milestone%20Project%202%20-%20Assignment.ipynb
 
 import random
+import sys
 
 """
 In this milestone project you will be creating a Complete BlackJack Card Game in Python.
@@ -18,13 +19,25 @@ And most importantly:
 You must use OOP and classes in some portion of your game. You can not just use functions in your game. Use classes to help you define the Deck and the Player's hand. There are many right ways to do this, so explore it well!
 Feel free to expand this game. Try including multiple players. Try adding in Double-Down and card splits! Remember to you are free to use any resources you want and as always:
 """
+
+## GLOBALS
+
 total_hands = 0
-dealer_wins = 0
-player1_wins = 0
-player2_wins = 0
-player1_hand = []
-player2_hand = []
-dealer_hand = []
+
+
+class Player():
+
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+        self.wallet = None
+        self.wins = 0
+        self.hands = 0
+        self.bet = 0
+
+
+    def bet(self, bet_amount):
+        self.wallet.withdrawl(bet_amount)
 
 
 class Account():
@@ -33,14 +46,19 @@ class Account():
         self.balance = amount
 
 
+
     def get_balance(self):
-        pass
+        return self.balance
 
     def deposit(self, amount):
-        return self.balance
+        self.balance += amount
 
 
     def withdrawl(self, amount):
+        if self.balance - amount < 0:
+            print("Withdrawl will result in negative balance - please select smaller amount")
+        else:
+            self.balance -= amount
 
 
 
@@ -59,9 +77,9 @@ class Deck():
     def shuffle_deck(self):
         pass
 
-    def display_hand(self, player, card_hand):
+    def display_hand(self, player_name, card_hand):
         total = 0
-        print(f"{player} has following hand:")
+        print(f"{player_name} has following hand:")
         for x in card_hand:
             print(f"\t {x[0]} of {x[1]}")
             total += x[2]
@@ -112,7 +130,7 @@ class Deck():
 class Blackjack(Deck):
 
     def __init__(self):
-        Deck.__init__(self) #NO COLON AND NO INDENT FOR PASS
+        Deck.__init__(self) #NO COLON AND NO INDENT FOR PASS  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         pass
 
 
@@ -126,12 +144,71 @@ class Blackjack(Deck):
 
 if __name__ == '__main__':
 
+    #Create Deck
     b = Blackjack()
     b.create_deck()
-    player1_hand = b.deal_hand(2)
-    print(b.count_hand(player1_hand))
-    b.display_hand("player1",player1_hand)
-    dealer_hand = b.deal_hand(2)
+
+    while True:
+        intro = input("Would you like to play a game of blackjack? [y|n]")
+        if intro == 'y':
+            break
+        elif intro == 'n':
+            sys.exit()
+        else:
+            print("Invalid selection...")
+
+
+    # List of Player Objects
+    players = []
+
+    while True:
+        player_count = input("How many players?")
+        for x in range(0,int(player_count)):
+            players.append(Player("Player" + str(x + 1),))
+        break
+
+
+    # Add 100 to each players account
+    for x in players:
+        x.wallet = Account(100)
+
+    print(f"Players: {players[0].name}")
+
+
+    while True:
+        bet_amount = input("Please place your bets..")
+        for x in range(0,int(player_count)):
+            print("Player{} please place your bet: ".format(x + 1))
+            players[x].wallet.withdrawl(int(bet_amount))
+        break
+
+
+    dealer_acct = Account(100)
+    dealer = Player("Dealer")
+    dealer.cards = b.deal_hand(2)
+
+    while True:
+        for x in range(0,int(player_count)):
+            players[x].cards = b.deal_hand(2)
+        break
+
+
+    while True:
+        for x in range(0,int(player_count)):
+            print("Player{} card count is {}".format(x + 1, b.count_hand(players[x].cards)))
+        break
+
+
+
+
+    #print("P1")
+    #print(p1.cards)
+    #print(b.count_hand(p1.cards))
+    #print(p1.wallet.balance)
+
+    #print(b.count_hand(player1_hand))
+    #b.display_hand("player1",player1_hand)
+    #dealer_hand = b.deal_hand(2)
     #print(b.count_hand(dealer_hand))
-    print(len(b.deck))
+    #print(len(b.deck))
 
