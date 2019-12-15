a52a815e5e2c315cc8dfcb577e3a7937064c5b67
rocket7 <test@test.com>
2019-12-14 19:22:53 -0500

    added geoJson data snf choropleth map

--- megaPyApp2/megaPyApp2.py
+++ megaPyApp2/megaPyApp2.py
@@ -31,7 +31,7 @@ def create_map(mapfile, markerfile):
     #map = folium.Map(location=[45,-71],zoom_start=6,tiles="Mapbox Bright")
     #map.add_child(folium.Marker(location=[45,-72], popup="loc1", icon=(folium.Icon(color="red"))))
 
-    fg = folium.FeatureGroup(name="Adam Map")
+    volcanos = folium.FeatureGroup(name="Volcano's")
     #os.chdir("megaPyApp2")
     fo=  open("Volcanoes.txt", "r")
     pdf = pandas.read_csv(fo)
@@ -58,7 +58,7 @@ def create_map(mapfile, markerfile):
 
     for i in volcano_list:
         iframe = folium.IFrame(html=html % (i[0],i[1]), width=200, height=100)
-        fg.add_child(folium.CircleMarker(location=(i[2],i[3]),popup=folium.Popup(iframe),icon=(folium.Icon(color=elev_color(i[1]))),tooltip=f"Name: {str(i[0])}"))
+        volcanos.add_child(folium.CircleMarker(location=(i[2],i[3]),popup=folium.Popup(iframe),radius=10,icon=(folium.Icon(color=elev_color(i[1]))),fill_color=elev_color(i[1]), color='grey',opacity=0.7,tooltip=f"Name: {str(i[0])}"))
         #fg.add_child(folium.Marker(location=[i[2],i[3]],popup=folium.Popup(iframe),icon=  (folium.Icon(color=elev_color(i[1]))),tooltip=f"Name: {str(i[0])}"))
         #fg.add_child(folium.Marker(location=[i[2],i[3]],popup=f"Elevation: {str(i[1])}",icon=(folium.Icon(color=elev_color(i[1]))),tooltip=f"Name: {str(i[0])}"))
         #map.add_child(folium.Marker(location=[i[2],i[3]],popup=str(i[0]),icon=(folium.Icon(color="green")),tooltip=(i[1])))
@@ -68,7 +68,14 @@ def create_map(mapfile, markerfile):
     #map.add_child(folium.Choropleth(location=[46,-71], popup="loc1", icon=(folium.Icon(color="blue"))))
     #map.add_child(folium.CircleMarker(   popup="loc1", icon=(folium.Icon(color="red"))))
     #map.add_child(folium.Circle(radius=10, popup="loc1", icon=(folium.Icon(color="green"))))
-    map.add_child(fg)
+
+    fg_overlay = folium.FeatureGroup("GeoJson Overlay")
+
+    fg_overlay.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read()),style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000 else "yellow" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+
+
+    map.add_child(volcanos)
+    map.add_child(fg_overlay)
     map.save(mapfile)
 
 
